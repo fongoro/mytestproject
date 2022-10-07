e3ab2ac928274b1c0ef4b386f55c3770bdc418ed
FaithOngoro <bfongoro@gmail.com>
2022-10-06 18:46:46 -0600

    just undated

--- Git/fay.sh
+++ Git/fay.sh
@@ -1,7 +1,1 @@
-read -p "please enter your name: " user_name
-read -p "please enter your preferred name : " prefer_name
-str_build="Hello $user_name"
-echo $str_build
-echo "you gave me "$prefer_name" as your prefer name"
-sleep 5
-echo "how will you like me to call you?"
+I AM KILLING IT :)
